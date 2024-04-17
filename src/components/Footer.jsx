6dd95c6cabc05e3f6e038cd54afb5fefb50290d7
project <>
2024-04-17 03:14:35 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='max-w-[1200px] sm:h-[150px] p-12 flex justify-between mx-auto'>
+        <span className='primary-color'>A. MME</span>
+        <p className='text-gray-600'>fotion.voe@goe.com <br /> #webDev2024</p>
+    </div>
+  )
+}
+
+export default Footer
