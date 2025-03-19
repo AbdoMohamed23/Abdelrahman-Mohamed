a407511672975756e777eb722a1755a20c170b67
Abdo Mohamed <bodimohamed2323@gmail.com>
2025-03-19 17:00:43 +0200

    second update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -25,7 +25,7 @@ const Work = () => {
                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                   Live
                 </button>
-              </a>
+              </a>movies
             </div>
           </div>
         </div>
@@ -77,7 +77,7 @@ const Work = () => {
           <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
             <span className='text-2xl font-bold text-white tracking-wider'>Movies</span>
             <div className='pt-8 text-center'>
-              <a href="https://movies-rep.netlify.app/">
+              <a href="https://movies-blush-phi.vercel.app/">
                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                   Live
                 </button>
